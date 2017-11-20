31300fbc2ffb608b2247e1639df02db493a8851b
Unknown <moyses15264123@gmail.com>
2017-11-19 22:04:52 -0300

    Tela de Login
    Tela de Login e Alteração no Manifest

---
+++ SisRU/app/src/main/java/tabian/com/actionbar/TelaLogin.java
@@ -0,0 +1,13 @@
+package tabian.com.actionbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_login);
+    }
+}
